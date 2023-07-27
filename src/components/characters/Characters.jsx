4ef436730e56c36d3c3a39ff18cf0552bd9141a3
Isabella <isabellachiaradia@hotmail.com>
2023-07-27 09:03:01 -0300

    feat: create 'characters' folder in 'components' This commit add the 'characters' forlder to the 'components' directory. The contents to the characters.jsx and .css files are extracted from the 'App.jsx' file to improve code organization.

---
+++ src/components/characters/Characters.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import axios from 'axios';
+import './Characters.css';
+
+const Characters = () => {
+  const [personajes, setPersonajes] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://gateway.marvel.com:443/v1/public/characters',
+          {
+            params: {
+              ts: '1',
+              apikey: '4033a8ca4f424d8ad5066be57a6e5a5e',
+              hash: '2ddbfdc5dd62b6adf7eefaf0c854ba0d',
+              limit: 100,
+            },
+          }
+        );
+
+        setPersonajes(response.data.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const sortedCharacters = useMemo(() => {
+    return [...personajes].sort((a, b) => a.name.localeCompare(b.name));
+  }, [personajes]);
+
+  const randomCharacters = useMemo(() => {
+    const shuffledCharacters = [...sortedCharacters].sort(() => 0.5 - Math.random());
+    return shuffledCharacters;
+  }, [sortedCharacters]);
+
+  const isValidThumbnail = (thumbnail) => {
+    return thumbnail && !thumbnail.path.includes('image_not_available');
+  };
+
+  const handleSearch = async (characterData) => {
+    if (characterData) {
+      setSearchResults([characterData]);
+    } else {
+      setSearchResults([]);
+    }
+  };
+
+  const handleSearchQuery = async () => {
+    if (searchQuery.trim() !== '') {
+      const characterData = await fetchCharacterByName(searchQuery);
+      handleSearch(characterData);
+    } else {
+      handleSearch(null);
+    }
+  };
+
+  return (
+    <div className="container">
+      {searchResults.length > 0 ? (
+        searchResults.map((per) => (
+          isValidThumbnail(per.thumbnail) && (
+            <div className="col" key={per.id}>
+              <div className="card">
+                <img
+                  src={`${per.thumbnail.path}.${per.thumbnail.extension}`}
+                  className="character-img"
+                  alt={`${per.name} image`}
+                />
+                <div className="card-body">
+                  <p className="character-name">{per.name}</p>
+                </div>
+              </div>
+            </div>
+          )
+        ))
+      ) : (
+        randomCharacters.map((per) => (
+          isValidThumbnail(per.thumbnail) && (
+            <div className="col" key={per.id}>
+              <div className="card">
+                <img
+                  src={`${per.thumbnail.path}.${per.thumbnail.extension}`}
+                  className="character-img"
+                  alt={`${per.name} image`}
+                />
+                <div className="card-body">
+                  <p className="character-name">{per.name}</p>
+                </div>
+              </div>
+            </div>
+          )
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Characters;
