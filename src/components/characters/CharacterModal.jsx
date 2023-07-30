c3cb7086fb87b4ef9d07726051c8414d5e10044e
Isabella <isabellachiaradia@hotmail.com>
2023-07-30 14:07:25 -0300

    feat: integrate FavoriteStar component in CharacterModal.

--- src/components/characters/CharacterModal.jsx
+++ src/components/characters/CharacterModal.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
+import FavoriteStar from '../FavoriteStar/FavoriteStar.jsx';
 import './CharacterModal.css';
 
-const CharacterModal = ({ character, onClose }) => {
+const CharacterModal = ({ character, onClose, favoriteComics, onToggleFavorite }) => {
+  const isComicInFavorites = (comic) => {
+    return favoriteComics.some((favComic) => favComic.id === comic.id);
+  };
+
+  const handleComicFavoriteToggle = (comic) => {
+    onToggleFavorite(comic);
+
+    console.log('Comics in favorites:', favoriteComics);
+  };
+
   return (
     <div className="character-modal">
       <div className="modal-content">
-        <span className="close-btn" onClick={onClose}>
+        <button className="close-btn" onClick={onClose}>
           &times;
-        </span>
+        </button>
         <h2>{character.name}</h2>
         <div className="comics-list">
           {character.comics.map((comic) => (
             <div key={comic.id} className="comic-item">
-              <img
-                src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
-                alt={comic.title}
-              />
+              <div className="comic-title-container">
+                <span className="comic-title">{comic.title}</span>
+                <FavoriteStar
+                  isFavorite={isComicInFavorites(comic)}
+                  onToggle={() => handleComicFavoriteToggle(comic)}
+                />
+              </div>
+              <img src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt={comic.title} />
               <div className="comic-details">
-                <h3>{comic.title}</h3>
                 <p>{comic.description || 'No description available'}</p>
               </div>
             </div>
