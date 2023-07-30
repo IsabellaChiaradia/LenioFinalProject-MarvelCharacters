d968118577a140575887475ca387b1ecf9ef4bde
Isabella <isabellachiaradia@hotmail.com>
2023-07-30 00:03:45 -0300

    feat(Characters): create Characters component and fetch all characters data.- Create characters.css for the characters component to achive the layout and design. - Added funcionalities for the real-time searching

--- src/components/characters/Characters.jsx
+++ src/components/characters/Characters.jsx
@@ -1,28 +1,38 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import axios from 'axios';
+import { useCharacters } from '../../context/context.jsx';
 import './Characters.css';
+import { SearchBar } from '../search-bar/SearchBar.jsx';
+import { useNavigate } from 'react-router-dom';
 
 const Characters = () => {
-  const [personajes, setPersonajes] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const { searchQuery, searchResults, setSearchResults } = useCharacters();
+  const [allCharacters, setAllCharacters] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          'https://gateway.marvel.com:443/v1/public/characters',
-          {
-            params: {
-              ts: '1',
-              apikey: '4033a8ca4f424d8ad5066be57a6e5a5e',
-              hash: '2ddbfdc5dd62b6adf7eefaf0c854ba0d',
-              limit: 100,
-            },
-          }
-        );
+        const totalPages = 16;
+        const characterRequests = [];
 
-        setPersonajes(response.data.data.results);
+        for (let page = 0; page < totalPages; page++) {
+          characterRequests.push(
+            axios.get('https://gateway.marvel.com:443/v1/public/characters', {
+              params: {
+                ts: '1',
+                apikey: '4033a8ca4f424d8ad5066be57a6e5a5e',
+                hash: '2ddbfdc5dd62b6adf7eefaf0c854ba0d',
+                limit: 100,
+                offset: page * 100,
+              },
+            })
+          );
+        }
+
+        const responses = await Promise.all(characterRequests);
+        const allCharactersData = responses.flatMap((response) => response.data.data.results);
+        setAllCharacters(allCharactersData);
       } catch (error) {
         console.log(error);
       }
@@ -31,12 +41,18 @@ const Characters = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    handleSearchQuery();
+  }, [searchQuery, allCharacters]);
+
   const sortedCharacters = useMemo(() => {
-    return [...personajes].sort((a, b) => a.name.localeCompare(b.name));
-  }, [personajes]);
+    return [...allCharacters].sort((a, b) => a.name.localeCompare(b.name));
+  }, [allCharacters]);
 
   const randomCharacters = useMemo(() => {
-    const shuffledCharacters = [...sortedCharacters].sort(() => 0.5 - Math.random());
+    const shuffledCharacters = [...sortedCharacters].sort(
+      () => 0.5 - Math.random()
+    );
     return shuffledCharacters;
   }, [sortedCharacters]);
 
@@ -44,29 +60,29 @@ const Characters = () => {
     return thumbnail && !thumbnail.path.includes('image_not_available');
   };
 
-  const handleSearch = async (characterData) => {
-    if (characterData) {
-      setSearchResults([characterData]);
+  const handleSearchQuery = () => {
+    if (searchQuery.trim() !== '') {
+      const filteredCharacters = allCharacters.filter((character) =>
+        character.name.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+      setSearchResults(filteredCharacters);
     } else {
-      setSearchResults([]);
+      setSearchResults(randomCharacters);
     }
   };
 
-  const handleSearchQuery = async () => {
-    if (searchQuery.trim() !== '') {
-      const characterData = await fetchCharacterByName(searchQuery);
-      handleSearch(characterData);
-    } else {
-      handleSearch(null);
-    }
+  const handleCharacterClick = (character) => {
+    const characterPath = encodeURIComponent(character.name);
+    navigate(`/${characterPath}`); // Navigate to the character's path URL
   };
 
   return (
     <div className="container">
+      <SearchBar />
       {searchResults.length > 0 ? (
-        searchResults.map((per) => (
-          isValidThumbnail(per.thumbnail) && (
-            <div className="col" key={per.id}>
+        searchResults.map((per) =>
+          isValidThumbnail(per.thumbnail) ? (
+            <div className="col" key={per.id} onClick={() => handleCharacterClick(per)}>
               <div className="card">
                 <img
                   src={`${per.thumbnail.path}.${per.thumbnail.extension}`}
@@ -78,12 +94,12 @@ const Characters = () => {
                 </div>
               </div>
             </div>
-          )
-        ))
+          ) : null
+        )
       ) : (
-        randomCharacters.map((per) => (
-          isValidThumbnail(per.thumbnail) && (
-            <div className="col" key={per.id}>
+        randomCharacters.map((per) =>
+          isValidThumbnail(per.thumbnail) ? (
+            <div className="col" key={per.id} onClick={() => handleCharacterClick(per)}>
               <div className="card">
                 <img
                   src={`${per.thumbnail.path}.${per.thumbnail.extension}`}
@@ -95,8 +111,8 @@ const Characters = () => {
                 </div>
               </div>
             </div>
-          )
-        ))
+          ) : null
+        )
       )}
     </div>
   );
