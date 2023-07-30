a10dd6dcc6a12789c7f32c9816db7a9d4d8c05b9
Isabella <isabellachiaradia@hotmail.com>
2023-07-30 00:05:17 -0300

    refactor(App): clean up code and move logic to the Characters component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
-import { SearchBar } from './components/search-bar/SearchBar.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CharactersProvider } from './context/context.jsx';
 import Characters from './components/characters/Characters.jsx';
 
 function App() {
   return (
     <div className="App">
-      <SearchBar />
-      <Characters />
+      <BrowserRouter>
+        <CharactersProvider>
+          <Routes>
+            <Route path="/" element={<Characters />} />
+            <Route path="/:character" element={<Characters />} />
+          </Routes>
+        </CharactersProvider>
+      </BrowserRouter>
     </div>
   );
 }
