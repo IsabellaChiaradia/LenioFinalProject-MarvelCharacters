0c845873bfec827a3add397e906b94d4f473114b
Isabella <isabellachiaradia@hotmail.com>
2023-07-29 23:53:37 -0300

    feat(SearchBar): update SearchBar component with real-time search. - Added Marvel logo and star icon to later be used for the favorites.

--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -1,26 +1,39 @@
 import React, { useState } from 'react';
-import { FaSearch } from "react-icons/fa";
+import marvelLogo from '../../assets/marvel-logo.png';
+import { BsStar } from 'react-icons/bs';
+import { CiSearch } from 'react-icons/ci';
 import './SearchBar.css';
+import { useCharacters } from '../../context/context.jsx';
+import { useNavigate } from 'react-router-dom';
 
-export const SearchBar = ({ onSearch }) => {
-  const [searchQuery, setSearchQuery] = useState('');
+export const SearchBar = () => {
+  const { setSearchQuery } = useCharacters();
+  const [localSearchQuery, setLocalSearchQuery] = useState('');
+  const navigate = useNavigate();
 
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      onSearch(searchQuery);
-    }
+  const handleChange = (event) => {
+    setLocalSearchQuery(event.target.value);
+    setSearchQuery(event.target.value);
+    navigate(`/${encodeURIComponent(event.target.value)}`); // Navigate to the search URL
   };
 
   return (
+    <div className="navbar">
+      <div className="logo-container">
+        <img src={marvelLogo} alt="Marvel Logo" className="marvel-logo" />
+        <span className="vertical-bar">|</span>
+      </div>
     <form className='input-container' onSubmit={(e) => e.preventDefault()}>
-      <FaSearch className="search-icon" />
+      <CiSearch className='search-icon' />
       <input
-        placeholder="Buscar"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        onKeyDown={handleKeyPress}
+        placeholder='Buscar'
+        value={localSearchQuery}
+        onChange={handleChange}
       />
     </form>
+    <BsStar className="star-icon"/>
+    </div>
   );
 };
+
+export default SearchBar;
