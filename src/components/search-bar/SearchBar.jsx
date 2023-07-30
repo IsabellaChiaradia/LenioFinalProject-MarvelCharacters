e87523a160ef244adff127ddd1bfd800441cae04
Isabella <isabellachiaradia@hotmail.com>
2023-07-30 20:09:59 -0300

    fix: Enable favoritesModal functionality in the Star icon of the searchbar component.

--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -5,16 +5,26 @@ import { CiSearch } from 'react-icons/ci';
 import './SearchBar.css';
 import { useCharacters } from '../../context/context.jsx';
 import { useNavigate } from 'react-router-dom';
+import FavoritesModal from '.././favoritesModal/FavoritesModal.jsx';
 
 export const SearchBar = () => {
-  const { setSearchQuery } = useCharacters();
+  const { setSearchQuery, favoriteComics } = useCharacters();
   const [localSearchQuery, setLocalSearchQuery] = useState('');
   const navigate = useNavigate();
+  const [isFavoritesModalOpen, setIsFavoritesModalOpen] = useState(false);
 
   const handleChange = (event) => {
     setLocalSearchQuery(event.target.value);
     setSearchQuery(event.target.value);
-    navigate(`/${encodeURIComponent(event.target.value)}`); // Navigate to the search URL
+    navigate(`/${encodeURIComponent(event.target.value)}`);
+  };
+
+  const handleFavoritesIconClick = () => {
+    setIsFavoritesModalOpen(true);
+  };
+
+  const handleCloseFavoritesModal = () => {
+    setIsFavoritesModalOpen(false);
   };
 
   return (
@@ -23,15 +33,18 @@ export const SearchBar = () => {
         <img src={marvelLogo} alt="Marvel Logo" className="marvel-logo" />
         <span className="vertical-bar">|</span>
       </div>
-    <form className='input-container' onSubmit={(e) => e.preventDefault()}>
-      <CiSearch className='search-icon' />
-      <input
-        placeholder='Buscar'
-        value={localSearchQuery}
-        onChange={handleChange}
-      />
-    </form>
-    <BsStar className="star-icon"/>
+      <form className='input-container' onSubmit={(e) => e.preventDefault()}>
+        <CiSearch className='search-icon' />
+        <input
+          placeholder='Buscar'
+          value={localSearchQuery}
+          onChange={handleChange}
+        />
+      </form>
+      <BsStar className="star-icon" onClick={handleFavoritesIconClick} />
+      {isFavoritesModalOpen && (
+        <FavoritesModal onClose={handleCloseFavoritesModal} />
+      )}
     </div>
   );
 };
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -7,9 +7,19 @@ export const useCharacters = () => useContext(CharactersContext);
 export const CharactersProvider = ({ children }) => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [favoriteComics, setFavoriteComics] = useState([]); // Assuming favoriteComics is defined here.
 
   return (
-    <CharactersContext.Provider value={{ searchQuery, setSearchQuery, searchResults, setSearchResults }}>
+    <CharactersContext.Provider
+      value={{
+        searchQuery,
+        setSearchQuery,
+        searchResults,
+        setSearchResults,
+        favoriteComics, // Make sure to include the favoriteComics in the context value.
+        setFavoriteComics, // If you have a setter for favoriteComics, add it to the context value as well.
+      }}
+    >
       {children}
     </CharactersContext.Provider>
   );
